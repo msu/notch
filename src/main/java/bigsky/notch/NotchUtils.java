33e75c06841d2061b75abc5b172978f97aff6abe
Dillon Shaffer <46535284+Molkars@users.noreply.github.com>
2025-08-21 15:52:11 -0600

    Layout work on Sunny; Notch Tokens

--- src/main/java/bigsky/notch/NotchUtils.java
+++ src/main/java/bigsky/notch/NotchUtils.java
@@ -1,5 +1,7 @@
 package bigsky.notch;
 
+import static bigsky.utils.BigSkyUtils.repr;
+
 public class NotchUtils {
     public static boolean isIdentifierStartChar(char first) {
         if (Character.isAlphabetic(first)) return true;
@@ -34,40 +36,6 @@ public class NotchUtils {
     public static void requireIdentifier(String token, String message) throws IllegalArgumentException {
         if (!isIdentifier(token)) {
             throw new IllegalArgumentException(message + ": the token " + repr(token) + " is not an identifier");
-        }
-    }
-
-    public static String repr(String s) {
-        final var out = new StringBuilder(s.length() + 2);
-        out.append('"');
-        for (char c : s.toCharArray()) {
-            repr(out, c);
-        }
-        out.append('"');
-        return out.toString();
-    }
-
-    public static String repr(char c) {
-        var out = new StringBuilder(3);
-        out.append("'");
-        repr(out, c);
-        out.append("'");
-        return out.toString();
-    }
-
-    private static void repr(StringBuilder sb, char c) {
-        if (c == '"') sb.append("\\\"");
-        else if (c == '\'') sb.append("\\'");
-        else if (c == '\\') sb.append("\\\\");
-        else if (c == '\b') sb.append("\\b");
-        else if (c == '\n') sb.append("\\n");
-        else if (c == '\t') sb.append("\\t");
-        else if (c == '\f') sb.append("\\f");
-        else if (c == '\r') sb.append("\\r");
-        else if (c < 32 || c > 0x7F) {
-            sb.append("\\u{").append(Integer.toHexString(c)).append("}");
-        } else {
-            sb.append(c);
         }
     }
 }
