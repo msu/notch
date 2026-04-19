53c76dc20628dcd0668e68b5162268668b884f8c
Carson Gross <carson@bigsky.software>
2026-04-19 10:24:06 -0600

    clean up logging from jackknife

--- src/main/java/bigsky/notch/logging/NotchLoggingServiceProvider.java
+++ src/main/java/bigsky/notch/logging/NotchLoggingServiceProvider.java
@@ -4,50 +4,74 @@ import org.slf4j.ILoggerFactory;
 import org.slf4j.IMarkerFactory;
 import org.slf4j.helpers.BasicMDCAdapter;
 import org.slf4j.helpers.BasicMarkerFactory;
-import org.slf4j.helpers.NOPMDCAdapter;
+import org.slf4j.helpers.NOPLoggerFactory;
 import org.slf4j.spi.MDCAdapter;
 import org.slf4j.spi.SLF4JServiceProvider;
 
-import java.util.logging.Logger;
+import java.util.ServiceLoader;
 
 public class NotchLoggingServiceProvider implements SLF4JServiceProvider {
 
-        /**
-         * Declare the version of the SLF4J API this implementation is compiled against.
-         * The value of this field is modified with each major release.
-         */
-        // to avoid constant folding by the compiler, this field must *not* be final
-        public static String REQUESTED_API_VERSION = "2.0.99"; // !final
+    /**
+     * Declare the version of the SLF4J API this implementation is compiled against.
+     * The value of this field is modified with each major release.
+     */
+    // to avoid constant folding by the compiler, this field must *not* be final
+    public static String REQUESTED_API_VERSION = "2.0.99"; // !final
 
-        private ILoggerFactory loggerFactory;
-        private IMarkerFactory markerFactory;
-        private MDCAdapter mdcAdapter;
+    private ILoggerFactory loggerFactory;
+    private IMarkerFactory markerFactory;
+    private MDCAdapter mdcAdapter;
 
-        public ILoggerFactory getLoggerFactory() {
-            return loggerFactory;
-        }
+    public ILoggerFactory getLoggerFactory() {
+        return loggerFactory;
+    }
 
-        @Override
-        public IMarkerFactory getMarkerFactory() {
-            return markerFactory;
-        }
+    @Override
+    public IMarkerFactory getMarkerFactory() {
+        return markerFactory;
+    }
 
-        @Override
-        public MDCAdapter getMDCAdapter() {
-            return mdcAdapter;
-        }
+    @Override
+    public MDCAdapter getMDCAdapter() {
+        return mdcAdapter;
+    }
 
-        @Override
-        public String getRequestedApiVersion() {
-            return REQUESTED_API_VERSION;
-        }
+    @Override
+    public String getRequestedApiVersion() {
+        return REQUESTED_API_VERSION;
+    }
 
-        @Override
-        public void initialize() {
+    @Override
+    public void initialize() {
+        markerFactory = new BasicMarkerFactory();
+        mdcAdapter = new BasicMDCAdapter();
+
+        NotchLogging.Mode mode = NotchLogging.effectiveMode();
+        if (mode == NotchLogging.Mode.FORCE) {
             loggerFactory = NotchLogging.get();
-            markerFactory = new BasicMarkerFactory();
-            mdcAdapter = new BasicMDCAdapter();
+            return;
         }
 
+        SLF4JServiceProvider other = findOtherProvider();
+        if (other != null) {
+            other.initialize();
+            loggerFactory = other.getLoggerFactory();
+            markerFactory = other.getMarkerFactory();
+            mdcAdapter = other.getMDCAdapter();
+        } else if (mode == NotchLogging.Mode.DISABLED) {
+            loggerFactory = new NOPLoggerFactory();
+        } else {
+            loggerFactory = NotchLogging.get();
+        }
+    }
 
+    private SLF4JServiceProvider findOtherProvider() {
+        for (var p : ServiceLoader.load(SLF4JServiceProvider.class)) {
+            if (!(p instanceof NotchLoggingServiceProvider)) {
+                return p;
+            }
+        }
+        return null;
+    }
 }
