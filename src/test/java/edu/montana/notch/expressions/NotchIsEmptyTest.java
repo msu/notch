b56e7b7a1ae74652b96146382ed7a52619de7ad1
Craven <zachp25@icloud.com>
2026-08-03 17:16:20 -0600

    fix test name

--- src/test/java/edu/montana/notch/expressions/NotchIsEmptyTest.java
+++ src/test/java/edu/montana/notch/expressions/NotchIsEmptyTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 import static edu.montana.notch.NotchTestUtils.eval;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class IsEmptyExpressionTest {
+class NotchIsEmptyTest {
 
     @Test
     void emptyString() {
